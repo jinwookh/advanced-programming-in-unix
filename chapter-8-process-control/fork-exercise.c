24233448aa456c7c48e12127480e915f9bf9056c
jinwook han <jin942002@naver.com>
2022-11-08 20:53:27 +0900

    add fork-exercise

---
+++ chapter-8-process-control/fork-exercise.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int globalvar =6;
+char buf[] = "a write to standard out\n";
+
+int main (void) {
+	int var;
+	pid_t pid;
+
+	var = 88;
+	if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf) - 1) {
+		printf("write error\n");
+		exit(1);
+	}
+	
+	if ((pid = fork()) < 0) {
+		printf("fork error\n");
+		exit(1);
+	} else if (pid == 0) {
+		globalvar ++;
+		var++;
+	} else {
+		sleep(2);
+	}
+
+	printf("pid = %ld, glob = %d, var = %d\n", (long)getpid(), globalvar, var);
+	exit(0);
+}
+
+
+
